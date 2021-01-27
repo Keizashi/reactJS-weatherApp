da0f737602fbd83a1903e25dc857fab8eecad6a9
Keizashi <suhoffpi-88@yandex.ru>
2021-01-27 18:42:39 +0400

    First and last upload :p

---
+++ src/components/info.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Info = props => (
+    <>
+        <h2>Погодное приложение</h2>
+        <p>Узнайте погоду в вашем городе!</p>
+    </>
+);
+
+export default Info;
